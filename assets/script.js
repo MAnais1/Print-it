c52028a54698b2d69e52faa1875cbb53fead113d
Anais Marmuse <marmuseanais@gmail.com>
2024-04-24 17:46:42 +0200

    modif fleche + point+ mise en place carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,65 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides)
+
+//les variables
+const dots = document.querySelector(".dots");
+let index = 0;
+const img = document.getElementById("image");
+const tag = document.getElementById("tagLine");
+const flecheDroite = document.getElementById("flecheDroite")
+const flecheGauche = document.getElementById("flecheGauche")
+
+
+
+//Les points
+function afficherPoints() {
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("div");
+		dot.classList.add("dot");
+		dots.appendChild(dot);
+		if (i == index) {
+			dot.classList.add("dot_selected");
+		}
+	}
+}
+afficherPoints()
+
+
+
+//clique droit
+function cliqueDroit() {
+	flecheDroite.addEventListener("click",()=>{
+		const tableauPoints = document.querySelectorAll(".dots .dot")
+		tableauPoints[index].classList.remove("dot_selected")
+		index++;
+		if (index > slides.length -1) {
+			index = 0;
+		}
+		tableauPoints[index].classList.add("dot_selected")
+		img.src = "./assets/images/slideshow/" + slides[index].image;
+		tag.textContent = slides[index].tagLine;
+		
+	})
+}
+cliqueDroit();
+ 
+
+//clique gauche
+function cliqueGauche() {
+	flecheGauche.addEventListener("click",()=>{
+		const tableauPoints = document.querySelectorAll(".dots .dot")
+		tableauPoints[index].classList.remove("dot_selected")
+		index--;
+		if (index < 0) {
+			index = slides.length - 1;
+		}
+		tableauPoints[index].classList.add("dot_selected")
+		img.src = "./assets/images/slideshow/" + slides[index].image;
+		tag.textContent = slides[index].tagLine;
+		
+	})
+}
+cliqueGauche();
+ 
